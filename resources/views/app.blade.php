e03364af1d7513beecd7bccf799c99f1dcf6e907
Roland Ahimbisibwe <rolandmbasa@gmail.com>
2024-02-08 04:43:22 +0300

    update document title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,6 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>Invoices</title>
     <meta name="description" content="Invoices app" />
     <link rel="icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
@@ -13,7 +12,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <title>{{ config('app.name', 'Invoices') }}</title>
+    <title>Timelogger App</title>
 
     <script>
       @auth
